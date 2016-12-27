74bb595177bd808e7afddd66a8128b9f6a2cbffc
gabriela <gabdelc@gmail.com>
2016-12-27 12:45:23 -0500

    base

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# posNeg
+# Ejercicio - 7
+
+### Inicio: realizar la suma de 10
+
+## Ingreso datos
+
+ + num1
+ + num2
+ + resultado
+ 
+## Proceso
+
+ + Pedir num1
+ + Pedír num2
+    + si - y +  se mostrara 1 
+    + si  no 0
++ si ambos son - = 1
++ si no 0 
+    -
+####Fin
---
+++ main.js
@@ -0,0 +1,17 @@
+function calcular(){
+    var num1 = parseFloat(document.getElementById("num_1").value);
+    var num2 = parseFloat(document.getElementById("num_2").value);
+    var salida = document.getElementById("salida");
+    var negativo = true;
+    
+        
+    if(   ){
+        salida.innerHTML = "1";
+    }else{
+        if (suma == 10){
+           salida.innerHTML = "1"; 
+        }else{
+            salida.innerHTML  = "0";
+        }
+    }
+}
